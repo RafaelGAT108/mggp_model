0fcd5f5f24a845289324832b7727f7b5d23060a8
Rafael <rafael.santos0t5@gmail.com>
2026-05-28 09:04:00 -0300

    mimo_data fixture included

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,3 +32,24 @@ def miso_data():
         y[k] = 0.3*y[k-1] + 0.1*u[k-1, 0] - 0.2*u[k-1, 1]
 
     return u, y
+
+@pytest.fixture
+def mimo_data():
+    """
+    Sistema MIMO simples:
+
+    y1[k] = 0.5*y1[k-1] + 0.2*u1[k-1] - 0.1*u2[k-1]
+    y2[k] = -0.3*y2[k-1] + 0.4*u1[k-1] + 0.1*u2[k-1]
+    """
+
+    n = 200
+
+    u = np.random.randn(n, 2)
+    y = np.zeros((n, 2))
+
+    for k in range(1, n):
+
+        y[k, 0] = (0.5 * y[k-1, 0] + 0.2 * u[k-1, 0] - 0.1 * u[k-1, 1])
+        y[k, 1] = (-0.3 * y[k-1, 1] + 0.4 * u[k-1, 0] + 0.1 * u[k-1, 1])
+
+    return u, y
