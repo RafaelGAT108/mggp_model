cb7a1e921e9d5fbb9e68ca04905058e0e448fcea
Rafael <rafael.santos0t5@gmail.com>
2026-05-28 07:50:58 -0300

    tests coverage for crossover

--- tests/test_crossovers.py
+++ tests/test_crossovers.py
@@ -1,13 +1,55 @@
 from mggp.base import Element
-from mggp.crossings import CrossLowUniform
+from mggp.crossings import CrossLowUniform, CrossLowOnePoint, CrossHighUniform, CrossHighOnePoint
 
-def test_crossover_execution():
+def test_crosslowuniform_execution():
     el = Element(nInputs=1, nOutputs=1, nTerms=3)
 
     ind1 = el.buildRandomModel()
     ind2 = el.buildRandomModel()
 
     cross = CrossLowUniform(el)
+
+    new1, new2 = cross.cross(ind1, ind2)
+
+    assert len(new1) == len(ind1)
+    assert len(new2) == len(ind2)
+
+
+def test_croslowonepoint_execution():
+    el = Element(nInputs=1, nOutputs=1, nTerms=3)
+
+    ind1 = el.buildRandomModel()
+    ind2 = el.buildRandomModel()
+
+    cross = CrossLowOnePoint(el)
+
+    new1, new2 = cross.cross(ind1, ind2)
+
+    assert len(new1) == len(ind1)
+    assert len(new2) == len(ind2)
+
+
+def test_croshighuniform_execution():
+    el = Element(nInputs=1, nOutputs=1, nTerms=3)
+
+    ind1 = el.buildRandomModel()
+    ind2 = el.buildRandomModel()
+
+    cross = CrossHighUniform(el)
+
+    new1, new2 = cross.cross(ind1, ind2)
+
+    assert len(new1) == len(ind1)
+    assert len(new2) == len(ind2)
+
+
+def test_crosshighonepoint_execution():
+    el = Element(nInputs=1, nOutputs=1, nTerms=3)
+
+    ind1 = el.buildRandomModel()
+    ind2 = el.buildRandomModel()
+
+    cross = CrossHighOnePoint(el)
 
     new1, new2 = cross.cross(ind1, ind2)
 
