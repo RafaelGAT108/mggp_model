21d21ff14d560cdde3263f55dc526707fbde852c
Rafael <rafael.santos0t5@gmail.com>
2026-05-28 07:51:18 -0300

    tests coverage for mutations

--- tests/test_mutations.py
+++ tests/test_mutations.py
@@ -1,11 +1,33 @@
 from mggp.base import Element
-from mggp.mutations import MutGPOneTree
+from mggp.mutations import MutGPOneTree, MutGPUniform, MutGPReplace
 
-def test_mutation_preserves_structure():
+def test_mutgponetree_preserves_structure():
     el = Element(nInputs=1, nOutputs=1, nTerms=3)
     ind = el.buildRandomModel()
 
     mut = MutGPOneTree(el)
+    new_ind, = mut.mutate(ind)
+
+    assert len(new_ind) == len(ind)
+
+
+
+def test_mutgpuniform_preserves_structure():
+    el = Element(nInputs=1, nOutputs=1, nTerms=3)
+    ind = el.buildRandomModel()
+
+    mut = MutGPUniform(el)
+    new_ind, = mut.mutate(ind)
+
+    assert len(new_ind) == len(ind)
+
+
+
+def test_mutgpraplace_preserves_structure():
+    el = Element(nInputs=1, nOutputs=1, nTerms=3)
+    ind = el.buildRandomModel()
+
+    mut = MutGPReplace(el)
     new_ind, = mut.mutate(ind)
 
     assert len(new_ind) == len(ind)
