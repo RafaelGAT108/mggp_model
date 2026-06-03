2ad96fdab7d6a2a48b82a402ca6b980573849ca1
Rafael <rafael.santos0t5@gmail.com>
2026-06-03 09:58:32 -0300

    test_croslowonepoint_mimo_execution and test_crosshighonepoint_mimo_execution included

--- tests/test_crossovers.py
+++ tests/test_crossovers.py
@@ -29,6 +29,20 @@ def test_croslowonepoint_execution():
     assert len(new2) == len(ind2)
 
 
+def test_croslowonepoint_mimo_execution():
+    el = Element(nInputs=2, nOutputs=2, nTerms=3, mode='MIMO')
+
+    ind1 = el.buildRandomModel()
+    ind2 = el.buildRandomModel()
+
+    cross = CrossLowOnePoint(el)
+
+    new1, new2 = cross.cross(ind1, ind2)
+
+    assert len(new1) == len(ind1)
+    assert len(new2) == len(ind2)
+
+
 def test_croshighuniform_execution():
     el = Element(nInputs=1, nOutputs=1, nTerms=3)
 
@@ -45,6 +59,20 @@ def test_croshighuniform_execution():
 
 def test_crosshighonepoint_execution():
     el = Element(nInputs=1, nOutputs=1, nTerms=3)
+
+    ind1 = el.buildRandomModel()
+    ind2 = el.buildRandomModel()
+
+    cross = CrossHighOnePoint(el)
+
+    new1, new2 = cross.cross(ind1, ind2)
+
+    assert len(new1) == len(ind1)
+    assert len(new2) == len(ind2)
+
+
+def test_crosshighonepoint_mimo_execution():
+    el = Element(nInputs=2, nOutputs=2, nTerms=3, mode='MIMO')
 
     ind1 = el.buildRandomModel()
     ind2 = el.buildRandomModel()
